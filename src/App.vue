ce18ccb1bcecaa13084acb5711561bc0b6c53936
Roberto Butti <roberto.butti@gmail.com>
2022-08-20 16:55:17 +0200

    update to Vite 3

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,18 @@
-<template>
-<!-- ##001: use the componennt in template -->
-  <RollTheDiceReactive />
-</template>
-
 <script>
 // ##002: import the component
-import RollTheDiceReactive from './components/RollTheDiceReactive.vue'
-
+import RollTheDice from './components/RollTheDice.vue'
 export default {
   name: 'App',
   components: {
     // ##003: declare the component
-    RollTheDiceReactive
+    RollTheDice
   }
 }
 </script>
+
+<template>
+<!-- ##001: use the component in template -->
+  <RollTheDice />
+</template>
+
+
